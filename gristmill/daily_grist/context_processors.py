2a1856c2fd8d206b1efdd6c12dfd712e9172bc42
taliesin <terracoda@gmail.com>
2014-03-24 00:00:34 -0230

    Revert "Initial commit of the gristmill project."
    This reverts commit fc64d9e5c34096eba32c096b2d71e524c28f1a6d.

--- gristmill/daily_grist/context_processors.py
+++
@@ -1,5 +0,0 @@
-from daily_grist.models import Entry
-
-def get_recent_entries(request):
-    recent_news_list = Entry.objects.order_by("-pub_date")[:1]  
-    return {'ENTRY_LIST': recent_news_list}
